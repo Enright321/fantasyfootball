1ebaf1c430d9b84d826bfb9ffcc3a28677517f05
Enright321 <enrightjoe4@gmail.com>
2021-10-25 15:01:39 -0500

    deployment prepare

--- backend/scripts/freeAgentQB.js
+++ backend/scripts/freeAgentQB.js
@@ -1,12 +1,14 @@
 const { Client } = require('espn-fantasy-football-api/node'); // node
 const fs = require('fs');
 const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const myClient = new Client({ leagueId: 81881363 });
 myClient.setCookies({
-  espnS2:
-    'AEAoBwOTz0IGGwf%2FVxW0M7BlXxJToo%2B0M2IbSZzjScKYN8vn7RbflCIj0MvAHfxfdqc3dOLQWnj5ux0BDj5aCqhX6Wck0XZX9%2FdwuXi1xe1MUiScYzXCrkPuwbQ7lyg9NhmYmZAcXA8YR7qnxM0dM8ypOxTgtf5IR1aG%2BrB0lucl3ensGYLZ9PaY1Xpht%2BA80SZIBVxrIG4MBDbA64czLafCkCAAbmPZ8KXZ2SEsL0A6PKJb5zOVO2y61URsPBp3Qy3Sk0nipoEIOK0wI2jcVU7S',
-  SWID: '{A67C2EF6-BF50-4B44-85A4-DBD6DCD98BE1}',
+  espnS2: process.env.espnS2_String,
+  SWID: process.env.swid_string,
 });
 
 const freeAgents = myClient.getFreeAgents({
