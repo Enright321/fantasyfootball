c8b986c08a672fb1ac606b76269141cf4ff7ba1e
Enright321 <enrightjoe4@gmail.com>
2021-10-30 16:31:08 -0500

    audit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { getFCP } from 'web-vitals';
+// import { getFCP } from 'web-vitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,4 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-getFCP(console.log);
+// getFCP(console.log);
